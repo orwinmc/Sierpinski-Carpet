b8caba7f3b756a3c76ffa2a278693d342238f45d
Michael Orwin <michaelxorwin@gmail.com>
2019-06-21 01:22:36 -0400

    another test

--- cpp/grid2.cpp
+++ cpp/grid2.cpp
@@ -425,10 +425,6 @@ int main() {
     harmonic_function(b, l, level, crosswires, "asdf");
 
 }
-<<<<<<< HEAD
-// test
-// test2
-=======
 
 // Test
->>>>>>> 58ae83980b7b34036742ae00f048c56bd46221c5
+// test again
